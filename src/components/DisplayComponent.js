8d2d4d27a6a9513eaa852e999117fd9382990826
Chad Yates <cayates1989@gmail.com>
2017-09-25 15:03:01 -0400

    another final with word wrap

--- src/components/DisplayComponent.js
+++ src/components/DisplayComponent.js
@@ -4,7 +4,7 @@ import React, { Component } from 'react';
 export default class DisplayComponent extends Component {
     render() {
       return (
-        <div>{this.props.sayWhat}</div>
+        <div className="wrap-me">{this.props.sayWhat}</div>
       );
     }
   }
